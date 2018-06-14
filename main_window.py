b7868bda63563c651321ad9102c1bf2f194464aa
Darek <dszalucki@wp.pl>
2018-06-14 22:01:11 +0200

    dodanie okna interfejsu

--- main_window.py
+++ main_window.py
@@ -8,26 +8,30 @@ def create_pesel():
 # tworzę główne okno
 root = tkinter.Tk()
 root.columnconfigure(3)
+root.title('ULTRA SUPER GENERATOR')
 
 # labelki
-pesel_label = tkinter.Label(master=root, text='PESEL')
-pesel_label.grid(row=0, column=0)
+pesel_label = tkinter.Label(master=root, text='PESEL:')
+pesel_label.grid(row=0, column=0, sticky=E)
 
-nip_label = tkinter.Label(master=root, text='NIP')
-nip_label.grid(row=1, column=0)
+nip_label = tkinter.Label(master=root, text='NIP:')
+nip_label.grid(row=1, column=0, sticky=E)
 
-regon_label = tkinter.Label(master=root, text='REGON')
-regon_label.grid(row=2, column=0)
+regon_label = tkinter.Label(master=root, text='REGON:')
+regon_label.grid(row=2, column=0, sticky=E)
 
 #
 pesel_entry = tkinter.Label(master=root)
-pesel_entry.grid(row=0, column=1, columnspan=2)
+pesel_entry.grid(row=0, column=1, padx=20, pady=10)
+pesel_entry.configure(bg='yellow', width=13, height=1, relief=SUNKEN)
 
-nip_entry = tkinter.Entry(master=root)
-nip_entry.grid(row=1, column=1, columnspan=2)
+nip_entry = tkinter.Label(master=root)
+nip_entry.grid(row=1, column=1, padx=20, pady=10)
+nip_entry.configure(bg='yellow', width=13, height=1, relief=SUNKEN)
 
-regon_entry = tkinter.Entry(master=root)
-regon_entry.grid(row=2, column=1, columnspan=2)
+regon_entry = tkinter.Label(master=root)
+regon_entry.grid(row=2, column=1, padx=20, pady=10)
+regon_entry.configure(bg='yellow', width=13, height=1, relief=SUNKEN)
 
 # buttony
 pesel_button = tkinter.Button(master=root, text='Generuj!', command=lambda: create_pesel())
@@ -39,5 +43,5 @@ nip_button.grid(row=1, column=3, columnspan=2)
 regon_button = tkinter.Button(master=root, text='Generuj!')
 regon_button.grid(row=2, column=3, columnspan=2)
 
-root.title('ULTRA SUPER GENERATOR')
+
 root.mainloop()
