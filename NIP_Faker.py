4f0704f810c5af42106a7dbc7f8a81c6e61a95ae
wpuchalski <wojciech.puchalski@blueservices.pl>
2018-06-15 15:03:03 +0200

    nip dalsza logika

--- NIP_Faker.py
+++ NIP_Faker.py
@@ -1,13 +1,22 @@
 import random
 
+
 def nip():
     us_number = random.randint(101, 998)
     if isinstance(us_number, int):
         us_number += 1
 
-    print(us_number)
-
-    random_number = random.randint()
-    control_number =
-
-    nip_number = us_number +
+    random_number = str(us_number) + str(random.randint(111111, 999999))
+    control_sum = [int(i) for i in str(random_number)]
+    balance_list = [6, 5, 7, 2, 3, 4, 5, 6, 7]
+    result = [a * b for a, b in zip(control_sum, balance_list)]
+    wynik = result / 11
+    if wynik == 10:
+        wynik += 1
+    results = list(map(int, result))
+    k = sum(results)
+    k = str(k)
+    num = k[1]
+    nip_final = str(results) + str(num)
+    return nip_final
+print(nip())
