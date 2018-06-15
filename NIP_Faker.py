ad400081873d06b60423b219413f381ff42d16d8
wpuchalski <wojciech.puchalski@blueservices.pl>
2018-06-15 09:28:43 +0200

    nip

---
+++ NIP_Faker.py
@@ -0,0 +1,13 @@
+import random
+
+def nip():
+    us_number = random.randint(101, 998)
+    if isinstance(us_number, int):
+        us_number += 1
+
+    print(us_number)
+
+    random_number = random.randint()
+    control_number =
+
+    nip_number = us_number +
