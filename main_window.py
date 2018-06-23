8843e71ab5382721074ce361d71141cc6013d57c
Darek <dszalucki@wp.pl>
2018-06-23 17:29:14 +0200

    Dodanie przycisku kopiuj do schowka

--- main_window.py
+++ main_window.py
@@ -7,6 +7,7 @@ from REGON_Faker import regon
 
 def create_pesel():
     pesel_entry.configure(text=f'{pesel()}')
+    print(str(pesel_entry))
 
 def create_nip():
     nip_entry.configure(text=f'{nip()}')
@@ -20,7 +21,7 @@ def copy_text_to_clipboard(new):
 
 # tworzę główne okno
 root = tkinter.Tk()
-root.columnconfigure(3)
+root.columnconfigure(5)
 root.title('ULTRA SUPER GENERATOR')
 
 # labelki
@@ -37,7 +38,6 @@ regon_label.grid(row=2, column=0, sticky=E)
 pesel_entry = tkinter.Label(master=root)
 pesel_entry.grid(row=0, column=1, padx=20, pady=10)
 pesel_entry.configure(bg='yellow', width=13, height=1, relief=SUNKEN)
-pesel_entry.bind("<Button-1>", copy_text_to_clipboard(new=pesel_entry))
 
 nip_entry = tkinter.Label(master=root)
 nip_entry.grid(row=1, column=1, padx=20, pady=10)
@@ -56,6 +56,18 @@ nip_button.grid(row=1, column=3, columnspan=2)
 
 regon_button = tkinter.Button(master=root, text='Generuj!', command=lambda: create_regon())
 regon_button.grid(row=2, column=3, columnspan=2)
+
+
+# kopiuj do schowka
+pesel_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=pesel_entry))
+pesel_copy.grid(row=0, column=5, columnspan=2)
+
+nip_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=nip_entry))
+nip_copy.grid(row=1, column=5, columnspan=2)
+
+regon_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=regon_entry))
+regon_copy.grid(row=2, column=5, columnspan=2)
+
 
 
 root.mainloop()
