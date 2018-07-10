b48768c36d80f50e55457dfc9b73133700d064c3
wpuchalski <wojciech.puchalski9@wp.pl>
2018-07-10 08:04:08 +0200

    drobne refacto

--- main_window.py
+++ main_window.py
@@ -7,7 +7,6 @@ from REGON_Faker import regon
 
 def create_pesel():
     pesel_entry.configure(text=f'{pesel()}')
-    print(str(pesel_entry))
 
 def create_nip():
     nip_entry.configure(text=f'{nip()}')
@@ -15,9 +14,9 @@ def create_nip():
 def create_regon():
     regon_entry.configure(text=f'{regon()}')
 
-def copy_text_to_clipboard(new):
+def copy_to_clipboard(new):
     root.clipboard_clear()  # clear clipboard contents
-    root.clipboard_append(new)  # append new value to clipbaord
+    root.clipboard_get()
 
 # tworzę główne okno
 root = tkinter.Tk()
@@ -34,7 +33,7 @@ nip_label.grid(row=1, column=0, sticky=E)
 regon_label = tkinter.Label(master=root, text='REGON:')
 regon_label.grid(row=2, column=0, sticky=E)
 
-#
+# style labelek
 pesel_entry = tkinter.Label(master=root)
 pesel_entry.grid(row=0, column=1, padx=20, pady=10)
 pesel_entry.configure(bg='yellow', width=13, height=1, relief=SUNKEN)
@@ -59,13 +58,13 @@ regon_button.grid(row=2, column=3, columnspan=2)
 
 
 # kopiuj do schowka
-pesel_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=pesel_entry))
+pesel_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_to_clipboard(new=pesel_entry))
 pesel_copy.grid(row=0, column=5, columnspan=2)
 
-nip_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=nip_entry))
+nip_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_to_clipboard(new=nip_entry))
 nip_copy.grid(row=1, column=5, columnspan=2)
 
-regon_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_text_to_clipboard(new=regon_entry))
+regon_copy = tkinter.Button(master=root, text='Kopiuj do schowka', command=lambda: copy_to_clipboard(new=regon_entry))
 regon_copy.grid(row=2, column=5, columnspan=2)
 
 
